ab7c36225563433eb6919b0a4ff87ecac30b6ebd
antPinot <pinot.anthony@gmail.com>
2025-12-10 12:10:06 +0100

    TP3

--- exercices/TP 03.sql
+++ exercices/TP 03.sql
@@ -1,9 +1,28 @@
 -- a. Listez toutes les données concernant les articles
+SELECT * FROM ARTICLES;
+
 -- b. Listez uniquement les références et désignations des articles de plus de 2 euros
+SELECT REF, DESIGNATION FROM ARTICLES WHERE PRIX > 2;
+
 -- c. En utilisant les opérateurs de comparaison, listez tous les articles dont le prix est compris entre 2 et 6.25 euros
+SELECT * FROM ARTICLES WHERE PRIX >= 2 AND PRIX <= 6.25;
+
 -- d. En utilisant l'opérateur BETWEEN, listez tous les articles dont le prix est compris entre 2 et 6.25 euros
+SELECT * FROM ARTICLES WHERE PRIX BETWEEN 2 AND 6.25;
+
 -- e. Listez tous les articles, dans l'ordre des prix descendants, et dont le prix n'est pas compris entre 2 et 6.25 euros et dont le fournisseur est Française d'Imports.
+SELECT * FROM ARTICLES WHERE NOT (PRIX BETWEEN 2 AND 6.25) AND ID_FOU = (SELECT * FROM FOURNISSEUR WHERE NOM = 'Française d''Imports')
+
 -- f. En utilisant un opérateur logique, listez tous les articles dont les fournisseurs sont la Française d'imports ou Dubois et Fils
+SELECT * FROM ARTICLES WHERE ID_FOU = (SELECT * FROM FOURNISSEUR WHERE NOM = 'Française d''Imports') OR ID_FOU = (SELECT * FROM FOURNISSEUR WHERE NOM = 'Dubois et Fils')
+
 -- g. En utilisant l'opérateur IN, réalisez la même requête que précédemment
+SELECT * FROM ARTICLES WHERE ID_FOU = (SELECT * FROM FOURNISSEUR WHERE NOM IN ('Française d''Imports', 'Dubois et Fils')
+
 -- h. En utilisant les opérateurs NOT et IN, listez tous les articles dont les fournisseurs ne sont ni Française d'Imports, ni Dubois et Fils.
+SELECT * FROM ARTICLES WHERE ID_FOU NOT IN (SELECT * FROM FOURNISSEUR WHERE NOM IN ('Française d''Imports', 'Dubois et Fils')
+
 -- i. Listez tous les bons de commande dont la date de commande est entre le 01/02/2019 et le 30/04/2019.
+SELECT * FROM BON WHERE DATE_CMDE BETWEEN '2019-02-01' AND '2019-04-30'
+
+
