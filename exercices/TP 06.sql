2a80bf5f04d3cd9aa2db3e9f9c5c789dba0ceaa2
antPinot <pinot.anthony@gmail.com>
2025-12-11 11:19:11 +0100

    TP6

--- exercices/TP 06.sql
+++ exercices/TP 06.sql
@@ -1,2 +1,12 @@
+SET
+SQL_SAFE_UPDATES = 0;
+
 -- a. Supprimer dans la table compo toutes les lignes concernant les bons de commande d'avril 2019
+DELETE
+FROM COMPO
+WHERE COMPO.BON_ID IN (SELECT BON_ID FROM BON WHERE MONTH (BON.DATE_CMDE) = 4 AND YEAR (BON.DATE_CMDE) = 2019);
+
 -- b. Supprimer dans la table bon tous les bons de commande d'avril 2019.
+DELETE
+FROM BON
+WHERE MONTH (BON.DATE_CMDE) = 4 AND YEAR (BON.DATE_CMDE) = 2019;
